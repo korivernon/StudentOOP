5c8fdef5906ab48b3be6c5b3bce1012f6edb426d
korivernon <44047473+korivernon@users.noreply.github.com>
2019-06-04 11:41:27 -0400

    committing

---
+++ my_code/pointers.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+    for(int i = 0; i < n;i++) {
+        i++;
+    }
+}
+
+
+void inc_with_pointer(int* iptr) {
+}
+
+
+void inc_with_reference(int& n) {
+}
+
+
+Thing** create_array_of_things(int n) {
+    Thing** thingsarray = new Thing*[n]; //  this allocates memory and creates an array of things
+    for(int i=0; i<n;i++){
+        thingsarray[i] = new Thing(i);
+    }
+    return thingsarray;
+}
+
+void print_all_things(Thing** things, int n) {
+}
+
+void double_all_things(Thing** things, int n) {
+    for(int i = 0; i < n ; i++){
+        things[i] -> val *=2; //this doubles every value by accesing it, then it changes
+                             //the value by multiplting it by two; this can also be done by
+                            // *(things[i]).val = i
+    }
+}
+
+void delete_all_things(Thing** things, int n) {
+    for(int i = 0; i < n; i++) {
+        delete things[i];
+    }
+    delete[] things;
+}
+
+
+void assignTA(Student& s, Student& ta) {
+}
+
+void printTAs(vector<Student> students) {
+}
+
