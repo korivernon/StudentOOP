b4892e821104024a491e81b92fae5e493cbb20e4
korivernon <44047473+korivernon@users.noreply.github.com>
2019-06-06 11:31:51 -0400

    weather header

--- my_code/weather.h
+++ my_code/weather.h
@@ -6,17 +6,28 @@
 struct GPS {
     double latitude;
     double longitude;
-    GPS(double la, double lo) : latitude(la), longitude(lo) {}
+    GPS(double la, double lo) : latitude(la),
+        longitude(lo) {}
 };
 
+std::ostream& operator<<(std::ostream& os, const GPS& gps);
+
+const int UNRATED = -1;
+const int BAD = 0;
+const int OK = 1;
+const int GOOD = 2;
+
 class Weather {
-    friend std::ostream& operator <<(std::ostream& os, const Weather& w);
+    friend std::ostream& operator<<(std::ostream& os, const Weather& w);
  public:
-    Weather(std::string nm, GPS loc); //loc signifies the locations
-    std::string get_name(); //this allows us to use get_name in the weather class
+    Weather(std::string nm, GPS loc);
+    std::string get_name() const;
+    int get_rating() const;
+    void set_rating(int new_rating);
  private:
     std::string station_nm;
     GPS my_loc;
+    int rating = UNRATED;
 };
 
 
