fdfa08218bdf193b9e445ebc0fc1b67559406206
korivernon <44047473+korivernon@users.noreply.github.com>
2019-06-27 19:36:48 -0400

    Latest

--- tests/test_tvector.cpp
+++ tests/test_tvector.cpp
@@ -15,7 +15,7 @@ const int VEC_SZ = 20;
 
 int main() {
     MyVec<int> iv = MyVec<int>();
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"~~~~~~~~works~~~~~~~~~"<<endl;
     for (int i = 0; i < VEC_SZ; i++) {
         iv.push_back(i);
         cout << iv[i]<<endl;
@@ -23,36 +23,42 @@ int main() {
     }
 
     assert(iv[VEC_SZ - 1] == VEC_SZ - 1);
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"~~~~~~~~~works~~~~~~~~"<<endl;
     for (int i = 0; i < iv.size(); i++) {
         iv[i] *= 2;
         cout<<iv[i]<<endl;
     }
     assert(iv[VEC_SZ - 1] == 38);
+    cout<<iv[VEC_SZ-1]<<" = 38"<<endl;
     MyVec<int> v3;
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"~~~~~~~~works~~~~~~~~~"<<endl;
     for (int i = 9; i < 100; i += 9) {
         v3.push_back(i);
         cout<<v3[i]<<endl;
     }
     assert(v3[1] == 18);
     print_vector(v3);
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"~~~~~~~~~works~~~~~~~~"<<endl;
     MyVec<int> v2 = iv;
     assert(v2 == iv);
     print_vector(v2);
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"~~~~~~~~~~works~~~~~~~"<<endl;
     v2 = v3;
     assert(v2 == v3);
     print_vector(v2);
-    cout<<"~~~~~~~~~~~~~~~~~"<<endl;
+    print_vector(v3);
+    cout<<"~~~~~~~~works~~~~~~~~~"<<endl;
+    cout<<"testing..."<<endl;
     for (int i = 0; i < v3.size(); i++) {
         assert((v3[i] % 9) == 0);
+        cout<<v3[i]<< " ";
     }
-    
-    MyVec<string> sv = MyVec<string>(10, "Hello");
+    cout<<endl;
+    cout<<"testing 'Hello'"<<endl;
+    MyVec<string> sv = MyVec<string>(10,"Hello");
     for (int i = 0; i < sv.size(); i++) {
         assert(sv[i] == "Hello");
     }
     print_vector(sv);
+    cout<<"test done "<<endl;
 }
